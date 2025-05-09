d63ea861c29e5baf445db9490e3cc0e62b8552fe
TranQuocTroq <tranquoctroq@gmail.com>
2025-05-09 15:24:28 +0700

    submit lab1

---
+++ Lab1/app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.app1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtResult;
+    Button btnSum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Ánh xạ Id cho accs biến giao diện
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtResult = findViewById(R.id.edtResult);
+        btnSum = findViewById(R.id.btnSum);
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtResult.setText(c+"");
+            }
+        });
+    }
+}
